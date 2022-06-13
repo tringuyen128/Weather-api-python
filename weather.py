b5a771d7d2a788a6eafc76940984f0a875895327
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-13 15:25:23 -0700

    done

--- weather.py
+++ weather.py
@@ -1,13 +1,12 @@
-from django.template import TemplateSyntaxError
 import requests
+import creds
 
 
-API_KEY = "1c2a0eb96be32f3071229665862e03df"
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 
 city = input("Enter a city name: ")
-request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+request_url = f"{BASE_URL}?appid={creds.API_KEY}&q={city}"
 response = requests.get(request_url)
 
 if response.status_code == 200:
